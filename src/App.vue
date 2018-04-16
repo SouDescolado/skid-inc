6c4577b1067bc93e48272e77160e6c227ab2c7cd
Thomas Cazade <cazade.thomas@gmail.com>
2018-04-16 15:47:13 +0200

    feat: added core-game loop logic

--- src/App.vue
+++ src/App.vue
@@ -26,8 +26,47 @@ import Type from './components/Type.vue';
   components: { Logs, Tabs, Type },
 })
 export default class App extends Vue {
+  /** 1000/fps */
+  private interval: number;
+  /** Last loop execution time, in ms */
+  private before: number;
+  /** Current loop time, in ms */
+  private now: number;
+  /** Frames-per-seconds, used in the interval */
+  private fps: number;
+  /** Core `loop` function reference */
+  private loop: number;
+
   constructor() {
     super();
+
+    this.before = new Date().getTime();
+    this.now = new Date().getTime();
+
+    this.fps = 30;
+    this.interval = 1000 / this.fps;
+  }
+
+  /** On game mounted, start the core-game loop */
+  public mounted(): void {
+    this.interval = window.setInterval(() => this.loop(), this.interval);
+  }
+
+  /** Core game loop */
+  public loop(): void {
+    this.now = new Date().getTime();
+
+    const elapsed = this.now - this.before;
+    const times = Math.floor(elapsed / this.interval);
+
+    (elapsed > this.interval) ? this.update(times) : this.update(1);
+
+    this.before = new Date().getTime();
+  }
+
+  /** Update called by the loop */
+  public update(times: number): void {
+    return;
   }
 }
 </script>
