b1b1439c36ada8f5163d228fd76753178c24a091
Thomas Cazade <cazade.thomas@gmail.com>
2018-04-18 14:12:03 +0200

    feat: added tabs, added scss variables and vue-tabs-component styling

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -1,5 +1,11 @@
 <template>
-  <div id="panel"></div>
+  <div id="panel">
+    <tabs>
+      <tab name="Scripts" prefix="<i class='fa fa-microchip'></i>"></tab>
+      <tab name="Stats" prefix="<i class='fa fa-bar-chart'></i>"></tab>
+      <tab name="Options" prefix="<i class='fa fa-cog'></i>"></tab>
+    </tabs>
+  </div>
 </template>
 
 <script lang="ts">
