451e3215e8d26d7cb3f4315a73894a7cfadcff60
Thomas Cazade <cazade.thomas@gmail.com>
2018-04-20 15:36:39 +0200

    fix(command-autocomplete): fix bug when it returns an empty string

--- src/utils/command-autocomplete.ts
+++ src/utils/command-autocomplete.ts
@@ -4,18 +4,19 @@ import * as Models from '../models';
 export const autocompleteCommand = (commands: Models.Command[], input: string[]): string => {
   const root = input[0];
   const args = input.slice(1, input.length);
+  const fullCmd = input.join(' ');
 
   /* If no arguments found, process the root only */
   if (args.length === 0) {
     /* If input is equal to nothing: return an empty string. Otherwise, find the command based on the root start */
-    if (!root.length) {
-      return '';
-    } else {
+    if (root.length) {
       const cmd = commands.find((el) => el.root.indexOf(root) === 0);
-      const cmdRoot = (cmd !== undefined) ? cmd.root : '';
+      const cmdRoot = (cmd !== undefined) ? cmd.root : fullCmd;
       return cmdRoot;
+    } else {
+      return fullCmd;
     }
   } else {
-    return '';
+    return fullCmd;
   }
 };
