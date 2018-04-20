624f6c44ddc2fe8c960bb6db257bd2464b75000d
Thomas Cazade <cazade.thomas@gmail.com>
2018-04-20 10:47:42 +0200

    feat(player): added panel-stats vue component and styling

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -2,7 +2,9 @@
   <div id="panel">
     <tabs>
       <tab name="Scripts" prefix="<i class='fa fa-microchip'></i>"></tab>
-      <tab name="Stats" prefix="<i class='fa fa-bar-chart'></i>"></tab>
+      <tab name="Stats" prefix="<i class='fa fa-bar-chart'></i>">
+        <panel-stats></panel-stats>
+      </tab>
       <tab name="Options" prefix="<i class='fa fa-cog'></i>"></tab>
     </tabs>
   </div>
@@ -11,7 +13,11 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
-@Component({})
+import PanelStats from './panels/PanelStats.vue';
+
+@Component({
+  components: { PanelStats },
+})
 export default class Panel extends Vue {
   constructor() {
     super();
---
+++ src/components/panels/PanelStats.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="panel-stats">
+    <div class="panel-stats-names">
+      <p>Level</p>
+      <p>Experience</p>
+      <p>Money</p>
+    </div>
+
+    <div class="panel-stats-values">
+      <p>{{level}}</p>
+      <p>{{exp}}/{{expReq}}</p>
+      <p>${{money}}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class Panel extends Vue {
+  constructor() {
+    super();
+  }
+
+  get money(): number {
+    return this.$store.getters.getMoney;
+  }
+
+  get totalMoney(): number {
+    return this.$store.getters.getTotalMoney;
+  }
+
+  get level(): number {
+    return this.$store.getters.getLevel;
+  }
+
+  get exp(): number {
+    return this.$store.getters.getExp;
+  }
+
+  get totalExp(): number {
+    return this.$store.getters.getTotalExp;
+  }
+
+  get expReq(): number {
+    return this.$store.getters.getExpReq;
+  }
+}
+</script>
+
+<style lang="scss"></style>
