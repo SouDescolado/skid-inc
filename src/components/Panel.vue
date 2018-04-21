880eb425d46d09ec9a688cccf42f49af08bce424
Thomas Cazade <cazade.thomas@gmail.com>
2018-04-21 11:26:19 +0200

    feat(scripts): added panel-scripts vue component, edited panel styling

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -1,10 +1,14 @@
 <template>
   <div id="panel">
     <tabs>
-      <tab name="Scripts" prefix="<i class='fa fa-microchip'></i>"></tab>
+      <tab name="Scripts" prefix="<i class='fa fa-microchip'></i>">
+        <panel-scripts></panel-scripts>
+      </tab>
+
       <tab name="Stats" prefix="<i class='fa fa-bar-chart'></i>">
         <panel-stats></panel-stats>
       </tab>
+
       <tab name="Options" prefix="<i class='fa fa-cog'></i>"></tab>
     </tabs>
   </div>
@@ -14,9 +18,10 @@
 import { Component, Vue } from 'vue-property-decorator';
 
 import PanelStats from './panels/PanelStats.vue';
+import PanelScripts from './panels/PanelScripts.vue';
 
 @Component({
-  components: { PanelStats },
+  components: { PanelStats, PanelScripts },
 })
 export default class Panel extends Vue {
   constructor() {
---
+++ src/components/panels/PanelScripts.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="panel-scripts">
+    <div class="panel-scripts-container" v-for="script in scripts">
+      <div class="panel-scripts-names">
+        <p>{{script.name}}</p>
+        <p>Level</p>
+        <p>Autoscript</p>
+      </div>
+
+      <div class="panel-scripts-values">
+        <p>{{script.progression}}s</p>
+        <p>{{script.level}}</p>
+        <p v-if="script.autoscript">unlocked</p>
+        <p v-else>locked</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+import * as Models from '../../models';
+
+@Component({})
+export default class Panel extends Vue {
+  constructor() {
+    super();
+  }
+
+  get scripts(): Models.Script[] {
+    return this.$store.getters.getScripts;
+  }
+}
+</script>
+
+<style lang="scss"></style>
