98738e5e4b84836031d74653f35e11b3aff1d19d
Thomas Cazade <cazade.thomas@gmail.com>
2018-04-22 15:32:00 +0200

    fix(scripts): disallow running a script with specific conditions

--- src/store/scripts/scripts.actions.ts
+++ src/store/scripts/scripts.actions.ts
@@ -16,8 +16,25 @@ export const actions = {
 
     if (!script) {
       context.dispatch('LOGS_PRINT', `<span class="error">error</span> cannot find a script with name <span class="sub">${scriptName}</span>, try <span class="sub">script -l</span>`);
+    } else if (!script.level) {
+      context.dispatch('LOGS_PRINT', `<span class="error">error</span> cannot start the <span class="sub">${script.name}</span> script because you haven't unlocked it`);
+    } else if (script.autoscript) {
+      context.dispatch('LOGS_PRINT', `<span class="warning">warn</span> it's useless to manually start <span class="sub">${script.name}</span> script because you have an autoscript for it`);
+    } else if (script.triggered) {
+      context.dispatch('LOGS_PRINT', `<span class="error">error</span> <span class="sub">${script.name}</span> script is already running, wait for it to finish`);
     } else {
       context.commit('scriptStart', scriptName);
+    }
+  },
+
+  /** Called from the game-loop update */
+  async SCRIPT_LOOP(context: ScriptContext, times: number) {
+    const scripts = context.state.scripts
+      .filter((scr) => scr.triggered || scr.autoscript)
+      .map((scr) => scr.name);
+
+    if (scripts.length) {
+      context.commit('scriptLoop', { scripts, times });
     }
   },
 };
