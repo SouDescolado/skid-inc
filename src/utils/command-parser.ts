da01c55aa706698472c330945ae275d0b376a4bf
Thomas Cazade <cazade.thomas@gmail.com>
2018-05-18 11:05:57 +0200

    fix(parser): added utils-flags and fix flag arguments length error

--- src/utils/command-parser.ts
+++ src/utils/command-parser.ts
@@ -1,5 +1,18 @@
 import { Command } from '../models';
 
+/**
+ *  Check for the --help or -h flag in the command passed by the user.
+ */
+export const hasHelpFlag = (args: string[]) => args.indexOf('--help') > -1 || args.indexOf('-h') > -1;
+
+/**
+ *  Check for the --list or -l flag in the command passed by the user.
+ */
+export const hasListFlag = (args: string[]) => args.indexOf('--list') > -1 || args.indexOf('-l') > -1;
+
+/**
+ *  Generate error message according to the specific command
+ */
 export const generateErrors = (command: Command): { [s: string]: string; } => {
   return {
     HELP_NOT_SUPPORTED: `<span class="error">error</span> <span class="sub">${command.root}</span> command doesn't support <span class="sub">-h</span> or <span class="sub">--help</span> flag`,
@@ -13,46 +26,63 @@ export const generateErrors = (command: Command): { [s: string]: string; } => {
   };
 };
 
-/** Check a command if it is valid and can be executed */
+/**
+ *  Check for a valid user input depending of the command detected.
+ */
 export const parseCommand = (command: Command, input: string[]) => {
   const types = generateErrors(command);
   const args = input.slice(1, input.length);
 
   const flags = checkFlags(command, args);
-  const argsLength = checkArgsLength(command, args);
+  const argsLength = checkArgsExists(command, args);
   const argsTypes = checkArgsTypes(command, args);
 
-  const errorMessage = (flags)
-    ? types[flags]
-    : (argsLength)
-      ? types[argsLength]
-      : (argsTypes)
-        ? types[argsTypes]
-        : null;
+  let errorMessage: string | undefined;
+
+  if (flags) {
+    errorMessage = types[flags];
+  } else if (!hasHelpFlag(args) && !hasListFlag(args)) {
+    if (argsLength) {
+      errorMessage = types[argsLength];
+    } else if (argsTypes) {
+      errorMessage = types[argsTypes];
+    }
+  }
 
   return {
-    errored: (errorMessage) ? true : false,
+    errored: !!errorMessage,
     errorCode: flags || argsLength || argsTypes,
     errorMessage,
   };
 };
 
-/** Check for `--help`/`-h` and `--list`/`-l` flags */
+/**
+ *  Check if user input contains any flags and if the command support these flags.
+ */
 const checkFlags = (command: Command, args: string[]): string | undefined => {
-  const hasHelp = args.indexOf('--help') > -1 || args.indexOf('-h') > -1;
-  const hasList = args.indexOf('--list') > -1 || args.indexOf('-l') > -1;
-
-  if (hasHelp && !command.help) {
+  if (hasHelpFlag(args) && !command.help) {
     return 'HELP_NOT_SUPPORTED';
   }
 
-  if (hasList && !command.list) {
+  if (hasListFlag(args) && !command.list) {
     return 'LIST_NOT_SUPPORTED';
   }
 };
 
-/** Check the amount of arguments passed */
-const checkArgsLength = (command: Command, args: string[]): string | undefined => {
+/**
+ *  Check if command support arguments, arguments missing/not required. Check arguments length
+ *  vs length expected and check if arguments exist for a specific command.
+ *
+ *  - Loop on all arguments objects
+ *    - Try to find a possibility with the first user argument
+ *      - If not found, argument at first potision doesn't exist for this command
+ *    - Check if user passed too much arguments (nested argObj or args.length)
+ *    - Check if user forgot an argument (nested argObj is present, no args at second position)
+ *    - Try to find a possibility in a nested argObj
+ *      - If not found, argument at second position doesn't exist for this command
+ *    - Break the loop since we don't want to try on other argObj
+ */
+const checkArgsExists = (command: Command, args: string[]): string | undefined => {
   if (args.length && !command.args && !command.arguments) {
     return 'ARGUMENTS_NOT_REQUIRED';
   }
@@ -61,16 +91,7 @@ const checkArgsLength = (command: Command, args: string[]): string | undefined =
     return 'ARGUMENTS_REQUIRED';
   }
 
-  /**
-   * - Loop on all arguments objects
-   * - If the first argument matches a possibility, we have found the right argument object
-   * - If the user entered a second argument but we don't have a nested-argument object, there is too much arguments
-   * - If the user haven't entered a second argument but we have a nested-argument object, there is too few arguments
-   * - Break the loop since we don't want to loop over other arguments objects
-   */
   if (command.args && command.arguments) {
-    const hasHelp = args.indexOf('--help') > -1 || args.indexOf('-h') > -1;
-    const hasList = args.indexOf('--list') > -1 || args.indexOf('-l') > -1;
     let iterations = 0;
 
     for (const argObj of command.arguments) {
@@ -87,14 +108,16 @@ const checkArgsLength = (command: Command, args: string[]): string | undefined =
         }
 
         break;
-      } else if (iterations >= command.arguments.length && !hasHelp && !hasList) {
+      } else if (iterations >= command.arguments.length) {
         return 'ARGUMENT_DOESNT_EXIST';
       }
     }
   }
 };
 
-/** Check the arguments types */
+/**
+ *  Check for valid arguments type, using `parseInt` and `isNaN`.
+ */
 const checkArgsTypes = (command: Command, args: string[]) => {
   for (const arg of args) {
     if (!command.arguments) {
@@ -104,9 +127,9 @@ const checkArgsTypes = (command: Command, args: string[]) => {
     const argType = (isNaN(parseInt(arg, 10))) ? 'string' : 'number';
 
     /**
-     * - Loop on all arguments
-     * - Find the possibility in an argument
-     * - If can't find a possibility, find another one on the nested argument
+     * - Loop on all arguments objects
+     * - Find the possibility in an argObj
+     * - If can't find a possibility, find another one on the nested argObj
      */
     for (const argumentObj of command.arguments) {
       const findPossibility = argumentObj.possibilities.indexOf(arg);
