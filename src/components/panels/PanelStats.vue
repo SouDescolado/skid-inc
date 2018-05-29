39c61f7411a60744959fe4c428a218ea1013e080
Thomas Cazade <cazade.thomas@gmail.com>
2018-05-29 11:44:25 +0200

    feat(stats): show player global multipliers

--- src/components/panels/PanelStats.vue
+++ src/components/panels/PanelStats.vue
@@ -13,11 +13,27 @@
         <p>${{money}}</p>
       </div>
     </div>
+
+    <div class="panel-container">
+      <div class="panel-names">
+        <p>Money mult.</p>
+        <p>Exp. mult.</p>
+        <p>Time mult.</p>
+      </div>
+
+      <div class="panel-values">
+        <p>x{{moneyMultiplier}}</p>
+        <p>x{{expMultiplier}}</p>
+        <p>/{{timeMultiplier}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+
+import * as Utils from '../../utils';
 
 @Component({})
 export default class Panel extends Vue {
@@ -47,6 +63,22 @@ export default class Panel extends Vue {
 
   get expReq(): number {
     return this.$store.getters.getExpReq;
+  }
+
+  get multipliers() {
+    return this.$store.getters.getMultipliers;
+  }
+
+  get moneyMultiplier() {
+    return Utils.format(this.multipliers.money);
+  }
+
+  get expMultiplier() {
+    return Utils.format(this.multipliers.exp);
+  }
+
+  get timeMultiplier() {
+    return Utils.format(this.multipliers.time);
   }
 }
 </script>
